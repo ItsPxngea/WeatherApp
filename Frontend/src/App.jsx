7ebf98dae997e86a101edb88d671123f68612396
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-07-13 10:12:59 +0200

    Called the searchbar so as to be able to see it on the website

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,9 @@
 import { useState, useRef, useEffect } from "react";
-/*import {
-  Sun, Cloud, CloudRain, CloudSnow, CloudLightning, CloudDrizzle,
-  Wind, Droplets, Gauge, Eye, Sunrise, Sunset, Search, X, Plu
-} from "lucide-react";
- */
 import WeatherCard from "./components/WeatherCard";
+import SearchBar from "./components/SearchBar";
+
 //Fallback city if user denies location or anything else
 const FALLBACK_CITY = "Globe"
-
-
-/*const CONDITIONS = ["sun", "cloud", "partly-cloudy", "rain", "drizzle", "storm", "snow"];
-const CONDITION_LABELS = {
-  sun: "Clear",
-  cloud: "Cloudy",
-  "partly-cloudy": "Partly Cloudy",
-  rain: "Rainy",
-  drizzle: "Drizzle",
-  storm: "Thunderstorms",
-  snow: "Snowy",
-}*/
 
 //Each entry is {id,query}
 //'query' = {city: "globe"} OR {lat, lon}
@@ -54,7 +39,7 @@ export default function App() {
     });
 
   };
-  const removeCity = (cityName) => {
+  const removeCity = (id) => {
     setLocations((prev) => prev.filter((loc) => loc.id !== id));
   }
 
@@ -70,7 +55,7 @@ export default function App() {
         .scrollbar-hide::-webkit-scrollbar { display: none; }
       `}</style>
 
-      {/*<SearchBar onAddCity={addCity} />*/}
+      <SearchBar onAddCity={addCity} />
 
       {locations.length === 0 ? (
         <div className="f-mono text-sm mt-16" style={{ color: "#6E7887" }}>
@@ -79,7 +64,7 @@ export default function App() {
       ) : (
         <div
           ref={scrollRef}
-          className="w-full flex gap-5 overflow-x-auto snap-x snap-mandatory pb-4 scrollbar-hide"
+          className={`w-full flex gap-5 overflow-x-auto snap-x snap-mandatory pb-4 scrollbar-hide ${locations.length <= 5 ? "justify-center" : "justify-start"}`}
           style={{ scrollbarWidth: "none" }}
         >
           {locations.map((loc) => (
@@ -91,7 +76,8 @@ export default function App() {
             />
           ))}
         </div>
-      )}
-    </div>
+      )
+      }
+    </div >
   );
 }
