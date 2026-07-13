f5c362a834d92c4de75d74c1d5692ccf9729aa4a
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-07-13 11:06:36 +0200

    fixed possible typo for frontend link

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -16,7 +16,7 @@ if (!string.IsNullOrEmpty(apiKey))
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReact", policy=>
-    {policy.WithOrigins("https://weather-app-michaeljohnson5143.vercel.app/")
+    {policy.WithOrigins("https://weather-app-michaeljohnson5143.vercel.app")
         .AllowAnyHeader()
         .AllowAnyMethod();
         });
